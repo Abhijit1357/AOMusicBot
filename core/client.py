d928112ed3ade0a6191f5924b298bdab055ee63c
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2026-03-25 21:58:33 +0530

    Update client.py

--- core/client.py
+++ core/client.py
@@ -1,8 +1,8 @@
-from pyrogram import Client
-from pyrogram.enums import ParseMode
+from hydrogram import Client
+from hydrogram.enums import ParseMode
 from config import API_ID, API_HASH, BOT_TOKEN, STRING_SESSION
 
-# Bot Client: Isme workers badhane se commands fast execute hongi
+# Bot Client
 app = Client(
     "AO_Music_Bot",
     api_id=API_ID,
@@ -10,15 +10,15 @@ app = Client(
     bot_token=BOT_TOKEN,
     parse_mode=ParseMode.HTML,
     in_memory=True,
-    workers=40 # 🔥 Fast response ke liye workers add kiye
+    workers=40
 )
 
-# User Assistant: Isme workers badhane se streaming connection fast hoga
+# Assistant Client
 user = Client(
     "AO_User",
     api_id=API_ID,
     api_hash=API_HASH,
     session_string=STRING_SESSION,
     in_memory=True,
-    workers=20 # Assistant multitasking ke liye
+    workers=20
 )
