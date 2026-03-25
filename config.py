1f0eec446fb01828f2cc44a2c1cf11737eaa771c
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2026-03-25 21:59:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 
 # ========== BOT CONFIG ==========
-API_ID = int(os.getenv("API_ID", "22834593"))
-API_HASH = os.getenv("API_HASH", "f400bc1d1baeb9ae93014ce3ee5ea835")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "6666555316:AAEBEexXKkXy_l9eO414wGXYa4p907EXTPc")
-STRING_SESSION = os.getenv("STRING_SESSION", "BQFcbaEAj5NxCIU8FfUh9yTyJRus2cfcOENDLt3S5cBzU3GRdgEqu0aJsF830bwGDESnbk_9PHKwEDs8YQRvhbmvdxC71xZhJ34915RUl40FZkR3jhneWNkUEw_JAg0j_IMQb0tL3AtQme9Cn5jSFS5ixsJf_AMaGGkG8akwX69OJYO3-sgfxvKPT31aidz2OlS7IPTq842EdoioQTka8mxsLVgr8e7IrbZVMN-TJ5oPrEGrK8qfoLxlOjjO96IOoxDQOkUQ3zVUkArr02jB3CYPeZeB8zQqaS0xg3n-E2rq6VgeSEdlGOXIm29hhM_xk3wASEd69HIk9YU2TLofnzRR7hBxXwAAAAF5c5ovAA")
+API_ID = int(os.getenv("API_ID", "22091901"))
+API_HASH = os.getenv("API_HASH", "54b0cd5fb47a40265b197f1a110b20b8")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8628889832:AAGl2wiVQiEwH1YMkFnc4R4Ys-sKUfHpK4k")
+STRING_SESSION = os.getenv("STRING_SESSION", "AQFRGH0AZDBKRNad532JmrKKWjJsunU2j-cUM-ONLPdkuuVcERRXJo_1u-ahULkPzPiMvSjOIgOGbVa1OnnCmjcs1n9BXIByomrVtYTW_KzlKvCdpzl8R5g5f6W4KKIugfeZLVEmpl9KfJTtO5tSGh_dv1-K70KaXIDFA5XsShbY02XtSf5KibzTqKae_wm9t5ypzfS38CcCPPnANmSRlvRse-PGtdMTUbDfKzwQzL9mgQW9ZEl_YlrOdtd2j_FWQb4zjOFagUJ0CgaQw_qtGIBRmUsTBC-8IWkcDwiv_VYlLOp6hLFWQso2sRI0AuNNRgfXylGd6rycTT2khdZeD_1cLnZmqwAAAAH58KNUAA")
 
-BOT_NAME = "AO Music Bot"
+BOT_NAME = "✨Dᴀʀᴋ Aɴɢᴇʟ Music✨🤍"
 
 # ========== SETTINGS ==========
-OWNER_ID = int(os.getenv("OWNER_ID", "6148346742"))
+OWNER_ID = int(os.getenv("OWNER_ID", "5390485406"))
 # Yahan space hata dein "-100..." se pehle
-LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", "-1002134425165"))
+LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", "-1003886464823"))
 
 DOWNLOAD_DIR = "downloads"
 MAX_QUEUE = 20
